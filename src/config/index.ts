63d04a62d37f9176dc633544d81bb9ed174b09dc
Syukrie <syukrie77@gmail.com>
2025-09-20 18:18:08 +0700

    push speq

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,16 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Kasur Ortopedic SPEQ",
+  author: "SPEQ",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Kasur Kesehatan Ortopedic SPEQ.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/ab.png",
   navLinks: [
-    { text: "Experience", href: "#experience" },
-    { text: "Projects", href: "#projects" },
-    { text: "About", href: "#about" },
+    { text: "Manfaat", href: "#manfaat" },
+    { text: "Produk", href: "#produk" },
+    { text: "Tentang", href: "#tentang" },
   ],
   socialLinks: [
     { text: "Twitter", href: "https://github.com/immois/astro-zen" },
@@ -25,73 +25,58 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Kasur SPEQ",
+    specialty: "Kasur Kesehatan Ortopedic",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+      "Kasur kesehatan yang mampu menopang tubuh dengan kuat dan memberikan rasa nyaman saat tidur dan bangun tidur.",
     email: "example@email.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
+      company: "SPEQ",
+      position: "Kasur Ortopedic",
       startDate: "May 2018",
       endDate: "Sept 2020",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Mencegah sakit tulang punggung.",
+        "Sejuk Ketika digunakan pada udara sedikit panas.",
+        "Membahagiakan Keluarga.",
+        "Mampu memberi kenyamanan pada tidur anda.",
+        "Kenyamanan tidur sampai bertahun tahun",
       ],
     },
-    {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
-    },
+   
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
+      name: "SPEQ S100",
+      summary: "Kasur kesehatan yang memberikan kenyamanan tidur anda.",
       linkPreview: "/",
       linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      image: "/s100.jpg",
     },
     {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
+      name: "Kasru SPEQ S200",
+      summary: "Manfaat untuk tulang punggung dan pinggang anda.",
       linkPreview: "/",
       linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
+      image: "/s200.jpg",
     },
     {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
+      name: "Kasur SPEQ S300",
+      summary: "Rasakan kenyamanan tidur yang sesungguhnya.",
       linkPreview: "/",
       linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
+      image: "/s300.jpeg",
     },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
-
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+      Kasur Ortopedic SPEQ dibuat oleh tangan-tangan terampil yang memberikan kualitas terbaik untuk tidur anda.
+     
+      Aneka Busa Internasioanl menjadi produsen kasur SPEQ memiliki pengalaman yang panjang untuk menciptakan kasur kesehatan yang nyaman dan sehat.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/kasur-speq.jpg",
   },
 };
 
